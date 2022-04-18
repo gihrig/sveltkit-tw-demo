f021dc4ef346a3a1e56dee06523ba6fed1dbee3e
gihrig <glen@arkadias.com>
2022-04-18 12:33:23 -0700

    refactor(footer.svelte): move page footer into component

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="flex flex-col items-center justify-center py-2 sm:py-10">
+  <p>
+    visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+  </p>
+</footer>
+
+<style>
+</style>
