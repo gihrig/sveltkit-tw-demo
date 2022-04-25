30bb0f0df9d1272b520a1d59c3bf55987e7b9364
gihrig <glen@arkadias.com>
2022-04-25 10:58:15 -0700

    refactor(lib/Counter.svelte): convert div class="counter" to section

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -13,7 +13,7 @@
   }
 </script>
 
-<div class="counter">
+<section>
   <button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
     <svg aria-hidden="true" viewBox="0 0 1 1">
       <path d="M0,0.5 L1,0.5" />
@@ -32,17 +32,17 @@
       <path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
     </svg>
   </button>
-</div>
+</section>
 
 <style>
-  .counter {
+  section {
     display: flex;
     border-top: 1px solid rgba(0, 0, 0, 0.1);
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     margin: 1rem 0;
   }
 
-  .counter button {
+  section button {
     @apply text-text hover:bg-hoverbg;
     width: 2em;
     padding: 0;
