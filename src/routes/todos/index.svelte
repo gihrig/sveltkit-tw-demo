e923eef3445de5ae15729bcc4fd87a068de947e1
gihrig <glen@arkadias.com>
2022-04-26 22:49:40 -0700

    fix(routes/todos/index.svelte): restore TODO Done toggle element border

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -124,6 +124,7 @@
   }
 
   .done .toggle {
+    /* Show check mark when {todo.done} */
     background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
   }
 
@@ -161,7 +162,9 @@
   .todo button {
     width: 2em;
     height: 2em;
-    border: none;
+    /* TODO: After element, class, alpha sort 'border: none'
+    removes TODO done circle. Not needed? */
+    /* border: none; */
     background-color: transparent;
     background-position: 50% 50%;
     background-repeat: no-repeat;
