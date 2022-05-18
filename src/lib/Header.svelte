7ed912d435c1869a08d13c927718690944f9d8ff
gihrig <glen@arkadias.com>
2022-05-17 22:40:37 -0700

    refactor(lib/Header.svelte): move .corner styles from <style> block to in line 'class'

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,9 +11,14 @@
 </script>
 
 <header class="flex justify-between">
-  <div class="corner">
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src={logo} alt="SvelteKit" />
+  <!-- Left corner -->
+  <div class="h-12 w-12">
+    <a
+      class="flex h-full w-full items-center justify-center"
+      href="https://kit.svelte.dev"
+      target="_blank"
+    >
+      <img class="h-8 w-8 object-contain" src={logo} alt="SvelteKit" />
     </a>
   </div>
 
@@ -21,7 +26,9 @@
     <svg class="m-0 block h-12 justify-end" viewBox="0 0 2 3" aria-hidden="true">
       <path class="fill-navbg" d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
     </svg>
-    <ul>
+    <ul
+      class="relative m-0 flex h-12 list-none items-center justify-center bg-navbg bg-contain p-0"
+    >
       {#each links as { name, path }}
         <li class="pointer relative h-full" class:active={$page.url.pathname === path}>
           <a
@@ -39,26 +46,7 @@
     </svg>
   </nav>
 
-  <div class="corner">
+  <div class="h-12 w-12">
     <!-- TODO put something else here? github link? -->
   </div>
 </header>
-
-<style>
-  ul {
-    @apply relative m-0 h-12 list-none p-0;
-    @apply flex items-center justify-center bg-navbg bg-contain;
-  }
-
-  .corner {
-    @apply h-12 w-12;
-  }
-
-  .corner a {
-    @apply flex h-full w-full items-center justify-center;
-  }
-
-  .corner img {
-    @apply h-8 w-8 object-contain;
-  }
-</style>
